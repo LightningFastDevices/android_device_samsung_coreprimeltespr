ad3d55ff99194f63cabe87bd1b2dfde62b98f397
Vincent Zvikaramba <zvikaram@teach.cs.toronto.edu>
2018-09-24 20:54:08 -0400

    coreprimeltespr: Update AndroidProducts makefile
    Consider {lineage,rr,omni,pa}_coreprimeltespr the AOSP way
    
    Change-Id: Ibc32cf1001cb5af41cbf3e103d8497aa74cba194

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,3 +1,3 @@
-LOCAL_PATH := device/samsung/gprimeltespr
+LOCAL_PATH := device/samsung/coreprimeltespr
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/omni_gprimeltespr.mk
+	$(LOCAL_DIR)/lineage_coreprimeltespr.mk
