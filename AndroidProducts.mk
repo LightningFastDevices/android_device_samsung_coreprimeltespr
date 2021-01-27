6aab9be4619ac8c8e38083b2ee7311415a491e94
zeeshan <itexpert120@gmail.com>
2021-01-27 20:17:58 +0500

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,3 +1,8 @@
 LOCAL_PATH := device/samsung/coreprimeltespr
 PRODUCT_MAKEFILES := \
 	$(LOCAL_DIR)/lineage_coreprimeltespr.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_coreprimeltespr-eng \
+    lineage_coreprimeltespr-userdebug \
+    lineage_coreprimeltespr-user
